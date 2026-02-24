57748895c7946282c5041fd579516b02f385cdbf
Gayatri Patil <gmpatil9913@gmail.com>
2026-02-24 07:29:51 +0530

    Fetching API Key From News API and understood state in class based componets and this state and this props in react

--- src/components/News.js
+++ src/components/News.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
+    articles = [
+
+    ]
+    constructor(){
+    super();
+    this.state = {
+      articles: this.articles,
+      loading: false
+    }
+  }
   render() {
     return (
-      <div>
-        This is a news component.
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+      <div className='container my-3'>
+        <h2>NewZy - Top Headlines</h2>
+        <div className='row'>
+        <div classname='col-md-4'>
+         <NewsItem title="myTitle" description= "mydesc"imageUrl = "too" newsUrl= "too"/>
+        </div>
+        <div classname='col-md-4'>
+         <NewsItem title="myTitle" description= "mydesc"/>
+        </div>
+        <div classname='col-md-4'>
+         <NewsItem title="myTitle" description= "mydesc"/>
+        </div>
+        </div>
       </div>
     )
   }
