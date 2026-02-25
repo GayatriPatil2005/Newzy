feea0d4cc89f819f38e3ef53111fa096c8ecce27
Gayatri Patil <gmpatil9913@gmail.com>
2026-02-25 09:00:11 +0530

    used looping through an array in JSX and Using fetch API for http calls

--- src/components/News.js
+++ src/components/News.js
@@ -2,30 +2,42 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    articles = [
 
-    ]
     constructor(){
     super();
     this.state = {
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: true
+    }
+  }
+
+   async componentDidMount(){
+    try {
+      let url = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=3a99e00a05ce43e39ae8a49b61365462"
+      let Data =  await fetch(url);
+      if (!Data.ok) {
+        throw new Error('Failed to fetch news');
+      }
+      let parsedData =  await Data.json()
+      this.setState({articles: parsedData.articles, loading: false})
+    } catch (error) {
+      console.error('Error fetching news:', error);
+      this.setState({loading: false, articles: []});
     }
   }
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewZy - Top Headlines</h2>
+        <h1>NewZy - Top Headlines</h1>
+       
         <div className='row'>
-        <div classname='col-md-4'>
-         <NewsItem title="myTitle" description= "mydesc"imageUrl = "too" newsUrl= "too"/>
+        {this.state.articles.map((element)=>{
+         return <div className='col-md-4' key={element.url}>
+         <NewsItem   title={element.title?element.title:""} description= {element.description?element.description:""}imageUrl = {element.urlToImage} newsUrl= {element.url}/>
         </div>
-        <div classname='col-md-4'>
-         <NewsItem title="myTitle" description= "mydesc"/>
-        </div>
-        <div classname='col-md-4'>
-         <NewsItem title="myTitle" description= "mydesc"/>
-        </div>
+        })}
+
+
         </div>
       </div>
     )
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -9,19 +9,16 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{ width: "18rem" }}>
-  <img src={imageUrl} className="card-img-top" alt="..." />
-  <div className="card-body">
+  <img src={!imageUrl?"https://platform.ny.eater.com/wp-content/uploads/sites/6/2026/02/Tin-Building_T-Brasserie-920x518-1.jpg?quality=90&strip=all&crop=0,3.5062362287493,100,92.987527542501}":imageUrl} className='card-img-top' alt='...'/>
     <h5 className="card-title">{title}</h5>
     <p className="card-text">
      {description}
     </p>
-    <a href="/newsdetail" className="btn btn-primary">
+    <a href={newsUrl} target="_blank" className='btn btn-sm btn-primary'>
       Read More
     </a>
   </div>
 </div>
-
-      </div>
     )
   }
 }
